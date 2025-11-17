4b756038693a53d1a33904080fe68f433912b4c7
lsk <lsk@pp.com>
2025-11-17 21:30:43 +0800

    test

--- stream/sse.go
+++ stream/sse.go
@@ -2,8 +2,10 @@ package stream
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type DataProcessor func(data []byte) ([]byte, error)
@@ -48,4 +50,52 @@ func (s *SSEStream) Stream(ctx context.Context, w http.ResponseWriter, dataChan 
 			flusher.Flush()
 		}
 	}
+}
+
+func SSEDataProcessor(data []byte) ([]byte, error) {
+	// Convert data to SSE format: data: <content>\n\n
+	sseData := fmt.Sprintf("data: %s\n\n", string(data))
+	return []byte(sseData), nil
+}
+
+type SSEMessage struct {
+	ID    string
+	Event string
+	Data  string
+	Retry *int
+}
+
+func SSEMessageProcessor(data []byte) ([]byte, error) {
+	var msg SSEMessage
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return nil, err
+	}
+
+	var sseData strings.Builder
+
+	// Add ID field if provided
+	if msg.ID != "" {
+		sseData.WriteString(fmt.Sprintf("id: %s\n", msg.ID))
+	}
+
+	// Add Event field if provided
+	if msg.Event != "" {
+		sseData.WriteString(fmt.Sprintf("event: %s\n", msg.Event))
+	}
+
+	// Add Data field (always required)
+	dataLines := strings.Split(msg.Data, "\n")
+	for _, line := range dataLines {
+		sseData.WriteString(fmt.Sprintf("data: %s\n", line))
+	}
+
+	// Add Retry field if provided
+	if msg.Retry != nil {
+		sseData.WriteString(fmt.Sprintf("retry: %d\n", *msg.Retry))
+	}
+
+	// End message with double newline
+	sseData.WriteString("\n")
+
+	return []byte(sseData.String()), nil
 }
--- stream/sse_test.go
+++ stream/sse_test.go
@@ -3,6 +3,7 @@ package stream
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -124,6 +125,91 @@ func TestSSEStream_Stream(t *testing.T) {
 				return ch
 			}(),
 			expectedError: "write error",
+		},
+		{
+			name:      "SSEDataProcessor_SimpleMessages",
+			sse:       &SSEStream{Processor: SSEDataProcessor},
+			ctx:       context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 2)
+				ch <- []byte("Hello World")
+				ch <- []byte("Second message")
+				close(ch)
+				return ch
+			}(),
+			expectedResult: "data: Hello World\n\ndata: Second message\n\n",
+			checkHeaders:   true,
+		},
+		{
+			name:      "SSEDataProcessor_SpecialCharacters",
+			sse:       &SSEStream{Processor: SSEDataProcessor},
+			ctx:       context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 2)
+				ch <- []byte("Test with 中文 & symbols!@#")
+				ch <- []byte("Line 1\nLine 2")
+				close(ch)
+				return ch
+			}(),
+			expectedResult: "data: Test with 中文 & symbols!@#\n\ndata: Line 1\nLine 2\n\n",
+		},
+		{
+			name: "SSEMessageProcessor_CompleteMessages",
+			sse:  &SSEStream{Processor: SSEMessageProcessor},
+			ctx:  context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 2)
+				msg1 := SSEMessage{ID: "123", Event: "greeting", Data: "Hello World"}
+				msg2 := SSEMessage{ID: "456", Event: "update", Data: "Updated data"}
+				data1, _ := json.Marshal(msg1)
+				data2, _ := json.Marshal(msg2)
+				ch <- data1
+				ch <- data2
+				close(ch)
+				return ch
+			}(),
+			expectedResult: "id: 123\nevent: greeting\ndata: Hello World\n\nid: 456\nevent: update\ndata: Updated data\n\n",
+		},
+		{
+			name: "SSEMessageProcessor_WithRetry",
+			sse:  &SSEStream{Processor: SSEMessageProcessor},
+			ctx:  context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 1)
+				retry := 5000
+				msg := SSEMessage{ID: "retry-test", Event: "error", Data: "Connection lost", Retry: &retry}
+				data, _ := json.Marshal(msg)
+				ch <- data
+				close(ch)
+				return ch
+			}(),
+			expectedResult: "id: retry-test\nevent: error\ndata: Connection lost\nretry: 5000\n\n",
+		},
+		{
+			name: "SSEMessageProcessor_MultilineData",
+			sse:  &SSEStream{Processor: SSEMessageProcessor},
+			ctx:  context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 1)
+				msg := SSEMessage{ID: "multiline", Event: "chat", Data: "Line 1\nLine 2\nLine 3"}
+				data, _ := json.Marshal(msg)
+				ch <- data
+				close(ch)
+				return ch
+			}(),
+			expectedResult: "id: multiline\nevent: chat\ndata: Line 1\ndata: Line 2\ndata: Line 3\n\n",
+		},
+				{
+			name: "SSEMessageProcessor_ErrorHandling",
+			sse:  &SSEStream{Processor: SSEMessageProcessor},
+			ctx:  context.Background(),
+			dataChan: func() chan []byte {
+				ch := make(chan []byte, 1)
+				ch <- []byte("invalid json")
+				close(ch)
+				return ch
+			}(),
+			expectedError: "invalid character 'i' looking for beginning of value",
 		},
 	}
 
